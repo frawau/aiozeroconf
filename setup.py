f5be6fd4d47d79139a4ce22d14864633616943a2
Francois Wautier <francois@wautier.eu>
2017-11-07 10:55:20 +0700

    More setup cleanup

--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@ from os.path import abspath, dirname, join
 
 from setuptools import setup
 
-PROJECT_ROOT = abspath(dirname(__file__))
-with open(join(PROJECT_ROOT, 'README.rst'), encoding='utf-8') as f:
-    readme = f.read()
 
 version = (
     [l for l in open(join(PROJECT_ROOT, 'aiozeroconf/aiozeroconf.py')) if '__version__' in l][0]
@@ -17,17 +14,16 @@ version = (
 )
 
 setup(
-    name='aiozeroconf',
+    packages=['aiozeroconf'],
     version=version,
+    author='Paul Scott-Murphy, William McBrine, Jakub Stasiak, François Wautier',
+    author_email='francois@wautier.eu',
     description='Pure Python Multicast DNS Service Discovery Library for asyncio '
     '(Bonjour/Avahi compatible)',
-    long_description=readme,
-    author='François Wautier, Paul Scott-Murphy, William McBrine, Jakub Stasiak',
     url='https://github.com/frawau/aiozeroconf',
-    py_modules=['aiozeroconf'],
+    download_url='https://github.com/frawau/aiozeroconf/archive/'+version+'.tar.gz',
     platforms=['unix', 'linux', 'osx'],
     license='LGPL',
-    zip_safe=False,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -40,18 +36,13 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython'
     ],
     keywords=[
         'Bonjour', 'Avahi', 'Zeroconf', 'Multicast DNS', 'Service Discovery',
         'mDNS', 'asyncio',
     ],
     install_requires=[
-        # netifaces 0.10.5 has a bug that results in all interfaces' netmasks
-        # to be 255.255.255.255 on Windows which breaks things. See:
-        # * https://github.com/jstasiak/python-zeroconf/issues/84
-        # * https://bitbucket.org/al45tair/netifaces/issues/39/netmask-is-always-255255255255
-        'netifaces!=0.10.5'
+        'netifaces',
     ],
 )
