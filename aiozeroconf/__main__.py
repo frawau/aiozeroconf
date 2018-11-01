3cfecafdd710a0c4aecf0a45e27e8bc0743df065
Jonathan Martin <homerunisgood@hotmail.com>
2018-11-01 23:37:50 +0100

    reformat main and wait for extra info before printing added events (and as such avoid mixing up lines in the console)

--- aiozeroconf/__main__.py
+++ aiozeroconf/__main__.py
@@ -33,13 +33,15 @@ from aiozeroconf import ServiceBrowser, ServiceStateChange, Zeroconf
 
 
 def on_service_state_change(zc, service_type, name, state_change):
-    print("Service %s of type %s state changed: %s" % (name, service_type, state_change))
-
     if state_change is ServiceStateChange.Added:
-        asyncio.ensure_future(on_service_state_change_process(zc,service_type,name))
+        asyncio.ensure_future(on_service_state_change_process(zc, service_type, name))
+    else:
+        print("Service %s of type %s state changed: %s" % (name, service_type, state_change))
 
-async def on_service_state_change_process(zc,service_type, name):
+
+async def on_service_state_change_process(zc, service_type, name):
     info = await zc.get_service_info(service_type, name)
+    print("Service %s of type %s state changed: %s" % (name, service_type, ServiceStateChange.Added))
     if info:
         print("  Address: %s:%d" % (socket.inet_ntoa(info.address), info.port))
         print("  Weight: %d, priority: %d" % (info.weight, info.priority))
@@ -54,19 +56,19 @@ async def on_service_state_change_process(zc,service_type, name):
         print("  No info")
     print('\n')
 
+
 async def do_close(zc):
     await zc.close()
 
 
-
 parser = argparse.ArgumentParser(description="Track and interact with Lifx light bulbs.")
-parser.add_argument('-i', "--iface",  default="",
+parser.add_argument('-i', "--iface", default="",
                     help="Name of the inteface to use.")
 parser.add_argument('-p', "--protocol", choices=['ipv4', 'ipv6', 'both'], default="ipv4",
                     help="What IP protocol to use.")
 parser.add_argument("-s", "--service", default="_http._tcp.local.",
                     help="The service to browse.")
-parser.add_argument("-d","--debug", action='store_true', default=False,
+parser.add_argument("-d", "--debug", action='store_true', default=False,
                     help="Set debug mode.")
 try:
     opts = parser.parse_args()
@@ -74,12 +76,11 @@ except Exception as e:
     parser.error("Error: " + str(e))
 
 if opts.protocol == "ipv4":
-    proto=[netifaces.AF_INET]
+    proto = [netifaces.AF_INET]
 elif opts.protocol == "ipv6":
-    proto=[netifaces.AF_INET6]
+    proto = [netifaces.AF_INET6]
 else:
-    proto=[netifaces.AF_INET,netifaces.AF_INET6]
-
+    proto = [netifaces.AF_INET, netifaces.AF_INET6]
 
 loop = asyncio.get_event_loop()
 logging.basicConfig(level=logging.CRITICAL)
@@ -87,7 +88,7 @@ if opts.debug:
     logging.getLogger('zeroconf').setLevel(logging.DEBUG)
     loop.set_debug(True)
 
-zc=Zeroconf(loop,proto,iface=opts.iface)
+zc = Zeroconf(loop, proto, iface=opts.iface)
 print("\nBrowsing services, press Ctrl-C to exit...\n")
 browser = ServiceBrowser(zc, opts.service, handlers=[on_service_state_change])
 try:
